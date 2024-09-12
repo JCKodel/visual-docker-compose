2f7bc0fc56007fee6c5e74ea07ed0902db9c39a8
J.C.Ködel <jckodel@gmail.com>
2024-09-12 18:34:38 -0300

    App theme

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:system_theme/system_theme.dart';
 
+import 'features/home.dart';
 import 'features/window_manager.dart';
+
+part 'main.main_app.dart';
 
 Future<void> main() async {
   await NativeWindowService.instance.initialize();
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World!"),
-        ),
-      ),
-    );
-  }
 }
