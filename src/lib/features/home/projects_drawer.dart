7a5bb1140f0093592a2a7175a4c9e007024457ad
J.C.Ködel <jckodel@gmail.com>
2024-09-12 21:54:12 -0300

    File picker

--- src/lib/features/home/projects_drawer.dart
+++ src/lib/features/home/projects_drawer.dart
@@ -1,11 +1,79 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hugeicons/hugeicons.dart';
+import 'package:path/path.dart' as Path;
 
 class ProjectsDrawer extends StatelessWidget {
   const ProjectsDrawer({required this.roundCorners, super.key});
 
   final bool roundCorners;
+
+  static Future<void> _createNewProject(BuildContext context) async {
+    final loc = AppLocalizations.of(context)!;
+
+    final path = await FilePicker.platform.getDirectoryPath(
+      dialogTitle: loc.createNewProject,
+    );
+
+    if (path == null || context.mounted == false) {
+      return;
+    }
+
+    final existingFile = File(Path.join(path, "docker-compose.yml"));
+
+    if (existingFile.existsSync()) {
+      showDialog<void>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(loc.createNewProject),
+          content: Text(loc.fileAlreadyExists),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(loc.ok),
+            ),
+          ],
+        ),
+      ).ignore();
+
+      return;
+    }
+
+    await _openDockerComposeFile(context, path, "docker-compose.yml");
+  }
+
+  static Future<void> _openExistingProject(BuildContext context) async {
+    final loc = AppLocalizations.of(context)!;
+
+    final pickedFile = await FilePicker.platform.pickFiles(
+      dialogTitle: loc.openExistingProject,
+      allowMultiple: false,
+      type: FileType.custom,
+      allowedExtensions: ["yml", "yaml"],
+      lockParentWindow: true,
+    );
+
+    if (pickedFile == null || context.mounted == false) {
+      return;
+    }
+
+    await _openDockerComposeFile(
+      context,
+      pickedFile.xFiles.first.path,
+      Path.basename(pickedFile.xFiles.first.name),
+    );
+  }
+
+  static Future<void> _openDockerComposeFile(
+    BuildContext context,
+    String path,
+    String fileName,
+  ) async {
+    print("Opening ${fileName} from ${path}");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +92,12 @@ class ProjectsDrawer extends StatelessWidget {
             backgroundColor: Colors.transparent,
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () => _createNewProject(context),
                 icon: const Icon(HugeIcons.strokeRoundedAddCircle),
+              ),
+              IconButton(
+                onPressed: () => _openExistingProject(context),
+                icon: const Icon(HugeIcons.strokeRoundedFolderOpen),
               ),
             ],
           ),
